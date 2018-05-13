6f2f46c2f77a765941f1a94b1d2a0db7f9668b64
Le Hai Ninh <ninh.contact@gmail.com>
2018-05-13 13:55:44 +0700

    Added textarea for simple input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.onTextChange = this.onTextChange.bind(this);
+  }
+
+  onTextChange(e) {
+    this.setState({
+      "json_text": e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +22,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <textarea style={{"height": "500px", "width": "500px"}}
+            onChange={this.onTextChange}
+          >
+            Insert JSON object
+          </textarea>
         </p>
       </div>
     );
